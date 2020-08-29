6cde2e974f45acc775d0b5c745280ddf741d7287
daybysay <yukita.sekai+github@gmail.com>
2020-08-29 14:44:28 +0900

    add swift file

---
+++ Study-XCFramework/Hoge.swift
@@ -0,0 +1,15 @@
+//
+//  Hoge.swift
+//  Study-XCFramework
+//
+//  Created by Takayuki Sei on 2020/08/29.
+//  Copyright © 2020 Takayuki Sei. All rights reserved.
+//
+
+import Foundation
+
+struct Hoge {
+    public func dump() -> String {
+        return "fuga"
+    }
+}
