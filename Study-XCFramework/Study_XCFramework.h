1c0ce3c97f22121e3f53dc8c7a817b62d099b501
daybysay <yukita.sekai+github@gmail.com>
2020-08-29 14:41:51 +0900

    initial commit

---
+++ Study-XCFramework/Study_XCFramework.h
@@ -0,0 +1,19 @@
+//
+//  Study_XCFramework.h
+//  Study-XCFramework
+//
+//  Created by Takayuki Sei on 2020/08/29.
+//  Copyright © 2020 Takayuki Sei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Study_XCFramework.
+FOUNDATION_EXPORT double Study_XCFrameworkVersionNumber;
+
+//! Project version string for Study_XCFramework.
+FOUNDATION_EXPORT const unsigned char Study_XCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Study_XCFramework/PublicHeader.h>
+
+
